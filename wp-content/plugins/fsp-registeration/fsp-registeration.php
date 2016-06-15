2aefc213a1ea2edab92d13712c5939dec393c50d
Ramkumar <ram1489@gmail.com>
2016-06-15 18:53:04 +0530

    User profile page table design removed

--- wp-content/plugins/fsp-registeration/fsp-registeration.php
+++ wp-content/plugins/fsp-registeration/fsp-registeration.php
@@ -385,90 +385,90 @@ function custom_user_profile_fields($current_user, $userdata,$user_ID){
     ?>
     <form name="profile" action="" method="post" enctype="multipart/form-data">
         <?php
-        echo wp_nonce_field('update-profile_' . $user_ID);
-        echo $user_ID;
+        wp_nonce_field('update-profile_' . $user_ID);
+        //echo $user_ID;
         ?>
         <input type="hidden" name="from" value="profile" />
         <input type="hidden" name="action" value="update" />
         <input type="hidden" name="checkuser_id" value="<?php echo $user_ID ?>" />
         <input type="hidden" name="dashboard_url" value="<?php echo get_option("dashboard_url"); ?>" />
         <input type="hidden" name="user_id" id="user_id" value="<?php echo $user_id; ?>" />
-        <table width="100%" cellspacing="0" cellpadding="0" border="0">
+        <ul>
             <?php
             if (isset($_GET['updated'])):
                 $d_url = $_GET['d'];
                 ?>
-                <tr>
-                    <td align="center" colspan="2"><span style="color: #FF0000; font-size: 11px;">Your profile changed successfully</span></td>
-                </tr>
+                <li>
+                    <div align="center" colspan="2"><span style="color: #FF0000; font-size: 11px;">Your profile changed successfully</span></div>
+                </li>
             <?php elseif ($errmsg != ""): ?>
-                <tr>
-                    <td align="center" colspan="2"><span style="color: #FF0000; font-size: 11px;"><?php echo $errmsg; ?></span></td>
-                </tr>
+                <li>
+                    <div align="center" colspan="2"><span style="color: #FF0000; font-size: 11px;"><?php echo $errmsg; ?></span></div>
+                </li>
     <?php endif; ?>
-            <tr>
-                <td colspan="2" align="center"><h2>User profile</h2></td>
-            </tr>
-            <tr>
-                <td>First Name</td>
-                <td><input type="text" name="first_name" id="first_name" value="<?php echo $userdata->first_name ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>Last Name</td>
-                <td><input type="text" name="last_name" class="mid2" id="last_name" value="<?php echo $userdata->last_name ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>Nick name <span style="color: #F00">*</span></td>
-                <td><input type="text" name="nickname" class="mid2" id="nickname" value="<?php echo $userdata->nickname ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>Email <span style="color: #F00">*</span></td>
-                <td><input type="text" name="email" class="mid2" id="email" value="<?php echo $userdata->user_email ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>New Password </td>
-                <td><input type="password" name="pass1" class="mid2" id="pass1" value="" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>New Password Confirm </td>
-                <td><input type="password" name="pass2" class="mid2" id="pass2" value="" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td align="right" colspan="2"><span style="color: #F00">*</span> <span style="padding-right:40px;">mandatory fields</span></td>
-            </tr>
-            <tr><td colspan="2"><h3>Extra profile information</h3></td></tr>
-            <tr>
-                <td>Facebook URL</td>
-                <td><input type="text" name="facebook" id="facebook" value="<?php echo esc_attr(get_the_author_meta('facebook', $userdata->ID)); ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>Twitter</td>
-                <td><input type="text" name="twitter" id="twitter" value="<?php echo esc_attr(get_the_author_meta('twitter', $userdata->ID)); ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>Date Of Birth</td>
-                <td><input type="text" name="dob" id="dob" value="<?php echo esc_attr(get_the_author_meta('dob', $userdata->ID)); ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>Phone</td>
-                <td><input type="text" name="phone" id="phone" value="<?php echo esc_attr(get_the_author_meta('phone', $userdata->ID)); ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>Address</td>
-                <td><input type="text" name="address" id="address" value="<?php echo esc_attr(get_the_author_meta('address', $userdata->ID)); ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>City</td>
-                <td><input type="text" name="city" id="city" value="<?php echo esc_attr(get_the_author_meta('city', $userdata->ID)); ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td>Postal Code</td>
-                <td><input type="text" name="postalcode" id="postalcode" value="<?php echo esc_attr(get_the_author_meta('postalcode', $userdata->ID)); ?>" style="width: 300px;" /></td>
-            </tr>
-            <tr>
-                <td align="center" colspan="2"><input type="submit" name="profile_submit" value="Update" /></td>
-            </tr>
-        </table>
+            <li>
+                <td colspan="2" align="center"><h2>User profile</h2></div>
+            </li>
+            <li>
+                <div>First Name</div>
+                <div><input type="text" name="first_name" id="first_name" value="<?php echo $userdata->first_name ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>Last Name</div>
+                <div><input type="text" name="last_name" class="mid2" id="last_name" value="<?php echo $userdata->last_name ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>Nick name <span style="color: #F00">*</span></div>
+                <div><input type="text" name="nickname" class="mid2" id="nickname" value="<?php echo $userdata->nickname ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>Email <span style="color: #F00">*</span></div>
+                <div><input type="text" name="email" class="mid2" id="email" value="<?php echo $userdata->user_email ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>New Password </div>
+                <div><input type="password" name="pass1" class="mid2" id="pass1" value="" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>New Password Confirm </div>
+                <div><input type="password" name="pass2" class="mid2" id="pass2" value="" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <td align="right" colspan="2"><span style="color: #F00">*</span> <span style="padding-right:40px;">mandatory fields</span></div>
+            </li>
+            <li><td colspan="2"><h3>Extra profile information</h3></div></li>
+            <li>
+                <div>Facebook URL</div>
+                <div><input type="text" name="facebook" id="facebook" value="<?php echo esc_attr(get_the_author_meta('facebook', $userdata->ID)); ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>Twitter</div>
+                <div><input type="text" name="twitter" id="twitter" value="<?php echo esc_attr(get_the_author_meta('twitter', $userdata->ID)); ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>Date Of Birth</div>
+                <div><input type="text" name="dob" id="dob" value="<?php echo esc_attr(get_the_author_meta('dob', $userdata->ID)); ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>Phone</div>
+                <div><input type="text" name="phone" id="phone" value="<?php echo esc_attr(get_the_author_meta('phone', $userdata->ID)); ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>Address</div>
+                <div><input type="text" name="address" id="address" value="<?php echo esc_attr(get_the_author_meta('address', $userdata->ID)); ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>City</div>
+                <div><input type="text" name="city" id="city" value="<?php echo esc_attr(get_the_author_meta('city', $userdata->ID)); ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div>Postal Code</div>
+                <div><input type="text" name="postalcode" id="postalcode" value="<?php echo esc_attr(get_the_author_meta('postalcode', $userdata->ID)); ?>" style="width: 300px;" /></div>
+            </li>
+            <li>
+                <div align="center" colspan="2"><input type="submit" name="profile_submit" value="Update" /></div>
+            </li>
+        </ul>
         <input type="hidden" name="update-profile-nonce" value="<?php echo wp_create_nonce('update-profile-nonce'); ?>"/>
     </form>
     <?php
