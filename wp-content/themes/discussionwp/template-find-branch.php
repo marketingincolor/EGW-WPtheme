9da9b4a25b2913b1ad2ab2292ca9d496c7fa6d35
rajasinghroot <rajasingh@farshore.com>
2016-06-15 22:09:02 +0530

    Implemented the villages with select box functionality

--- wp-content/themes/discussionwp/template-find-branch.php
+++ wp-content/themes/discussionwp/template-find-branch.php
@@ -14,22 +14,40 @@
     define('WP_USE_THEMES', false);
     require($path . 'wp-load.php');
     $blog_list = get_blog_list(0, 'all');
-    foreach ($blog_list AS $blog) {
-        echo "<li type='square'><a href='http://" . $blog['domain'] . $blog['path'] . "' target='_blank'>" . $blog['domain'] . $blog['path'] . "</a></li>";
-    }
+    //echo "<li type='square'><a href='http://" . $blog['domain'] . $blog['path'] . "' target='_blank'>" . $blog['domain'] . $blog['path'] . "</a></li>";
     ?>
 </div>
 <div class="white-popup-block">
     <div class="find-a-branch-container">
         <h4>Find A Branch</h4>
         <div class="fs-custom-select">
-            <select>
-                <option value="Select Your Village" selected="selected">Select Your Village</option>
-                <option value="saab">The village</option>
+            <select id="findavillage">
+                <option value="" selected="selected">Select Your Village</option>
+                <?php foreach ($blog_list AS $blog) { ?>
+                    <?php
+                    $getRel = explode('/', $blog['path']);
+
+                    print_r($getRel);
+                    ?>
+                    <option value="<?php echo "http://" . $blog['domain'] . $blog['path'] ?>"><?php echo $getRel[count($getRel) - 2]; ?></option>
+<?php } ?>
             </select>
         </div>
         <div class="ev-btn-container">
-            <button type="button">myEveergreen Home</button>
+            <button type="button" id="myevergreen">myEveergreen Home</button>
         </div>
     </div>
 </div>
+<script>
+    jQuery(document).ready(function () {
+        jQuery("#myevergreen").click(function () {
+            if (jQuery("#findavillage").val() == "") {
+                return false;
+            } else {
+                window.open(jQuery("#findavillage").val(), '_blank');
+            }
+
+        });
+    });
+
+</script>
