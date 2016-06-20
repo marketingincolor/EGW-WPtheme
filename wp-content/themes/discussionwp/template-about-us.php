2c5d718581748365605435f08584eb29b8095e70
node <node@node-Lenovo-G550>
2016-06-20 19:21:57 +0530

    Moved about us page design changes

--- wp-content/themes/discussionwp/template-about-us.php
+++ wp-content/themes/discussionwp/template-about-us.php
@@ -9,16 +9,22 @@ get_header();?>
     <div class="mkd-content-inner">
         <div class="mkd-container">
             <div class="mkd-container-inner clearfix">
+                <div class="about-us-container">
+                      <h2><?php the_title(); ?></h2>
+                    <div class="about-us-content">
                         <?php
                         the_content();
                         ?>
-                        <div class="fspgreen_btn">
+                    </div>
+                    <div class="fspgreen_btn">
                         <input type="submit" name="fspr_contact_submit" value="Contact Us">
-                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div> 
+</div>
+    
 <?php
 get_footer();
 ?>
