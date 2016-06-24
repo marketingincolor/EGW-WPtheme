679c9bf29c7057941a70576b0a4abf13d39a982d
FSSD <FSSD@FSSD-PC>
2016-06-24 19:20:21 +0530

    Completed single article page design updates and sidebar updates

--- wp-content/themes/discussionwp/template_sidebar_single.php
+++ wp-content/themes/discussionwp/template_sidebar_single.php
@@ -3,23 +3,23 @@
     <div class="mkd-section-title-holder clearfix">
         <span class="mkd-st-title">Share</span>
     </div>
-        <?php echo do_shortcode('[aps-counter theme="theme-2"]'); ?>  
+    <?php echo do_shortcode('[aps-counter theme="theme-2"]'); ?>  
 </div>
-
-<div class="mkd-ratings-holder">
-    <div class="mkd-ratings-text-holder">
-        <h6 class="mkd-ratings-text-title"><?php //esc_html_e('Rate This Article', 'discussionwp'); ?></h6>
-        <div class="mkd-ratings-stars-holder">
-            <?php
-            if (function_exists('the_ratings')) {
-                the_ratings();
-            }
-            ?>
+<div class="mkd-ratings-holder-container">
+    <div class="mkd-section-title-holder clearfix"><span class="mkd-st-title"><?php esc_html_e('Rate This Article', 'discussionwp'); ?></span></div>
+    <div class="mkd-ratings-holder">
+            <div class="mkd-ratings-stars-holder">
+                <?php
+                if (function_exists('the_ratings')) {
+                    the_ratings();
+                }
+                ?>
+            </div>
+     
+        <div class="mkd-ratings-message-holder">
+            <div class="mkd-rating-value"></div>
+            <div class="mkd-rating-message"></div>
         </div>
-    </div>
-    <div class="mkd-ratings-message-holder">
-        <div class="mkd-rating-value"></div>
-        <div class="mkd-rating-message"></div>
     </div>
 </div>
 <div class="widget mkd-rpc-holder">
