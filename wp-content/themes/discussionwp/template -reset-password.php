a7a8616d0099b8ce2a0afd8410cbeb798a651073
node <node@node-Lenovo-G550>
2016-06-27 21:30:00 +0530

    Moved reset password  page design

--- wp-content/themes/discussionwp/template -reset-password.php
+++ wp-content/themes/discussionwp/template -reset-password.php
@@ -8,20 +8,29 @@ Purpose - For reset password functionality
 ?>
 
 <?php get_header(); ?>
-<?php
-  echo '<input type="hidden" name="fp-ajax-nonce" id="fp-ajax-nonce" value="' . wp_create_nonce( 'fp-ajax-nonce' ) . '" />';
-?>
+
 <script type="text/javascript">
     var resetpassword_ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
 </script>
 
+<div class="login-container">
+    <h3 class="fspr_header">Reset Password</h3>
+    <div class="status"></div>
+    <form id="fspr_login_form"  class="fspr_form"action="" method="post">
+        <fieldset>
+            <ul>
+                <li>
+                    <label for="fspr_user_Login">Username</label>
+                    <input id="user_email" type="text" class="required" name="user_email">
+                </li>
+            </ul>
+            <p>
+                <input type="hidden" name="fp-ajax-nonce" id="fp-ajax-nonce" value="<?php echo wp_create_nonce('fp-ajax-nonce'); ?>"/>
+                <input id="fspr_login_submit" name="fspr_login_submit" type="button" value="Reset Password" class="fsplogin_btn submit_button" />
+            </p>
 
-<form id="forgot_password" class="ajax-auth" method="post">    
-    <h1>Reset Password</h1>
-    <p class="status"></p>  
-    <label for="user_email">E-mail</label>
-    <input id="user_email" type="text" class="required" name="user_email">
-    <input class="submit_button" type="button" value="Reset Password">
-</form>
+        </fieldset>
+    </form>
+</div>
 
 <?php get_footer(); ?>
