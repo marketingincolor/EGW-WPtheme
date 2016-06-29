8f542575325a153d1cd9ed1250665bdc5c0f15fa
FSSD <FSSD@FSSD-PC>
2016-06-29 20:00:29 +0530

    Completed single sidebar design for village site

--- wp-content/themes/village-child/functions.php
+++ wp-content/themes/village-child/functions.php
@@ -5,8 +5,7 @@ function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
 
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style)
-    );
+    wp_enqueue_style('fsp_custom_css_child', get_bloginfo( 'stylesheet_directory' ).'/assets/css/fspstyles_child.css');
 }
 
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
