e8dfcb8a5894f261e6f3fe50434f93557c890dd9
muthupandifsp <muthupandifsp@gmail.com>
2016-06-29 14:14:25 +0530

    update village sub site sidebar designs

---
+++ wp-content/themes/village-child/sidebar/template-ads.php
@@ -0,0 +1,13 @@
+<div class="widget mkd-rpc-holder">
+    <div class="widget widget_categories">
+        <div class="mkd-section-title-holder clearfix"><span class="mkd-st-title">Ads</span></div>
+        <div class="mkd-rpc-content">  
+            <?php if (is_front_page() || is_home() || is_404()) { ?>
+                <!--- Insert Ads here --->
+                <?php if (function_exists('drawAdsPlace')) drawAdsPlace(array('id' => 1), true); ?>
+                <!--- Ads end here --->
+                <?php }
+            ?>
+        </div>
+    </div>    
+</div>
---
+++ wp-content/themes/village-child/sidebar/template-welcome-to-villages.php
@@ -0,0 +1,10 @@
+<div class = "widget widget_categories village-sidebar-welcome-to-villages-widget">
+    <div class = "mkd-section-title-holder clearfix"><span class = "mkd-st-title">Welcome to The Villages</span></div>
+    <div class = "mkd-rpc-content village-sidebar-welcome-to-villages">        
+        <ul>
+            <li><a href = "<?php echo network_site_url(); ?>register">Join This Village?</a></li>
+            <li><a href = "#">Find Another Village</a></li>
+            <li><a href = "<?php echo network_site_url(); ?>">Return to Main Site</a></li>
+        </ul>    
+    </div>
+</div>
