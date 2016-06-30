acae3ab1ba520a3534357bd75ca6096fa80d84c3
node <node@node-Lenovo-G550>
2016-06-30 20:09:48 +0530

    Reorder videos single sidebar and single sidebar in mainsite

--- wp-content/themes/discussionwp/template_sidebar_single.php
+++ wp-content/themes/discussionwp/template_sidebar_single.php
@@ -5,6 +5,15 @@
     </div>
     <?php echo do_shortcode('[aps-counter theme="theme-2"]'); ?>  
 </div>
+<div class="widget mkd-rpc-holder">
+    <div class="widget widget_categories">
+        <div class="mkd-rpc-content">             
+            <!--- Insert Ads here --->
+            <?php if (function_exists('drawAdsPlace')) drawAdsPlace(array('id' => 1), true); ?>
+            <!--- Ads end here --->            
+        </div>
+    </div>
+</div>
 <div class="mkd-ratings-holder-container">
     <div class="mkd-section-title-holder clearfix"><span class="mkd-st-title"><?php esc_html_e('Rate This Article', 'discussionwp'); ?></span></div>
     <div class="mkd-ratings-holder">
@@ -19,16 +28,6 @@
         <div class="mkd-ratings-message-holder">
             <div class="mkd-rating-value"></div>
             <div class="mkd-rating-message"></div>
-        </div>
-    </div>
-</div>
-<div class="widget mkd-rpc-holder">
-    <div class="widget widget_categories">
-        <div class="mkd-section-title-holder clearfix"><span class="mkd-st-title">Ads</span></div>
-        <div class="mkd-rpc-content">             
-            <!--- Insert Ads here --->
-            <?php if (function_exists('drawAdsPlace')) drawAdsPlace(array('id' => 1), true); ?>
-            <!--- Ads end here --->            
         </div>
     </div>
 </div>
