bb6b4fe04e713c86e88b22127b76d9a9beff5faa
muthupandifsp <muthupandifsp@gmail.com>
2016-07-01 16:21:47 +0530

    updated validation to the comment form

--- wp-content/themes/discussionwp/assets/js/common.js
+++ wp-content/themes/discussionwp/assets/js/common.js
@@ -87,7 +87,53 @@ jQuery(document).ready(function ()
         if(jQuery('.fspr_register_error').length)
             e.preventDefault();
                         
-        });
+    });
+        
+    //Comment Form Validation    
+    jQuery('#commentform').submit(function (e) {
+       
+        
+        if(jQuery('.fspr_comment_error').length)
+            jQuery('.fspr_comment_error').remove();
+        
+        //Name field validation
+        if(jQuery('#commentform #author').length){
+            var name=jQuery('#commentform #author').val();
+            if(name==""){            
+                jQuery('<label class="fspr_comment_error">This field is required.</label>').insertAfter( '#commentform #author' );
+            }else if(name.length>40){
+                jQuery('<label class="fspr_comment_error">Name cannot be more than 40 characters.</label>').insertAfter( '#commentform #author' );
+            }else if(/[^a-zA-Z0-9\-]/.test( name )){
+                jQuery('<label class="fspr_comment_error">Name can only contain alphanumeric characters and hyphens(-).</label>').insertAfter('#commentform #author');
+            }
+        }
+        
+        //Email field validation
+        if(jQuery('#commentform #email').length){
+            var email=jQuery('#commentform #email').val();
+            
+            if(email==""){
+                jQuery('<label class="fspr_comment_error">This field is required.</label>').insertAfter( '#commentform #email' );                
+            }else {
+                var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+                if(!pattern.test(email)){
+                    jQuery('<label class="fspr_comment_error">This must be a valid email address.</label>').insertAfter( '#commentform #email' );
+                }
+            }
+            
+        }
+        
+        //Comment field validation
+        var comment=jQuery('#commentform #comment').val();
+        if(comment==""){
+            jQuery('<label class="fspr_comment_error">This field is required.</label>').insertAfter( '#commentform #comment' );
+        }
+        
+                        
+        if(jQuery('.fspr_comment_error').length)
+            e.preventDefault();
+    });
+        
 
 
 });
