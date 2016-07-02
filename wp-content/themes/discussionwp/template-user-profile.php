012b9251d194e5f90cd12806692fb89bc2df9468
Ramkumar <ram1489@gmail.com>
2016-07-02 18:06:20 +0530

    public profile updated and profile changes

--- wp-content/themes/discussionwp/template-user-profile.php
+++ wp-content/themes/discussionwp/template-user-profile.php
@@ -111,7 +111,7 @@ get_header();
                                                     ?>
                                                     <img src="<?php echo $attachment[0]; ?>" width="248"/>
                                                 <?php else : ?>                                                    
-                                                    <img src="wp-content/themes/discussionwp/assets/img/aavathar.jpg" width="248" height="248"/>
+                                                    <img src="<?php echo get_template_directory_uri();?>/assets/img/aavathar.jpg" width="248" height="248"/>
                                                 <?php endif; ?>
                                                 <div class="fspgray_btn">
                                                     <input type="file" id="userProfileImage" name="userProfileImage" style="display:none">                                                
