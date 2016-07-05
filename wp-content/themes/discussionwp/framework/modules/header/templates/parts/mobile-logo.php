608f925ae9aba66879b8686033416b26a1c71cae
FSSD <FSSD@FSSD-PC>
2016-07-05 16:48:56 +0530

    Completed header logo section and event page

--- wp-content/themes/discussionwp/framework/modules/header/templates/parts/mobile-logo.php
+++ wp-content/themes/discussionwp/framework/modules/header/templates/parts/mobile-logo.php
@@ -5,5 +5,14 @@
         <img src="<?php echo esc_url($logo_image); ?>" alt="<?php esc_html_e('mobile-logo','discussionwp'); ?>"/>
     </a>
 </div>
-
+<?php
+global $blog_id;
+if ($blog_id != 1) {
+   $current_site = get_current_site();?>
+   <div class="sub-site-logo">
+    <a href=""><h2><?php echo get_bloginfo('name'); ?></h2></a>
+</div>
+<?php
+} 
+?>
 <?php do_action('discussion_after_mobile_logo'); ?>
