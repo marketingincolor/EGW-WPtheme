52741cd6214a80d8616912e61715a472b8a2b603
rajasinghroot <rajasingh@farshore.com>
2016-07-07 16:01:05 +0530

    removed frontend restriction on the ads section

--- wp-content/themes/discussionwp/sidebar/template-ads.php
+++ wp-content/themes/discussionwp/sidebar/template-ads.php
@@ -1,12 +1,9 @@
 <div class="widget mkd-rpc-holder">
     <div class="widget widget_categories">
         <div class="mkd-rpc-content">  
-            <?php if (is_front_page() || is_home() || is_404()) { ?>
-                <!--- Insert Ads here --->
-                <?php if (function_exists('drawAdsPlace')) drawAdsPlace(array('id' => 1), true); ?>
-                <!--- Ads end here --->
-                <?php }
-            ?>
+            <!--- Insert Ads here --->
+            <?php if (function_exists('drawAdsPlace')) drawAdsPlace(array('id' => 1), true); ?>
+            <!--- Ads end here --->
         </div>
     </div>    
 </div>
