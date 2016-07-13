57de92500ff0017488eb7c9e703c5846d97c3dcc
rajasinghroot <rajasingh@farshore.com>
2016-07-13 18:08:09 +0530

    Updated the prevent condtion to not display the 'Join this the villages' option after getting the login page in subsite home page

--- wp-content/themes/discussionwp/sidebar/template-welcome-to-villages.php
+++ wp-content/themes/discussionwp/sidebar/template-welcome-to-villages.php
@@ -2,7 +2,9 @@
     <div class="fsp-branch-title"><h4>Welcome to The Villages!</h4></div>
     <div class="fsp-branch-content">        
         <ul>
+            <?php if (!is_user_logged_in()) { ?>
             <li><a href = "<?php echo network_site_url(); ?>register"><i class="fa fa-check" aria-hidden="true"></i> Join This The Villages?</a></li>
+            <?php } ?>
             <li class="welcome-my-pop"><a href = "<?php echo network_site_url(); ?>wp-content/themes/discussionwp/template-find-branch.php"><i class="fa fa-search" aria-hidden="true"></i> Find Another The Villages</a></li>
             <li><a href = "<?php echo network_site_url(); ?>"><i class="fa fa-reply" aria-hidden="true"></i> Return to Main Site</a></li>
         </ul>    
