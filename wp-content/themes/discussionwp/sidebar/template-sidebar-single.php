944fc104baa17b4a441c0ae6157ddcc389d4283e
node <node@node-Lenovo-G550>
2016-07-27 13:46:10 +0530

    Updated reorder widgets in video and article sidebars

--- wp-content/themes/discussionwp/sidebar/template-sidebar-single.php
+++ wp-content/themes/discussionwp/sidebar/template-sidebar-single.php
@@ -1,13 +1,11 @@
 <?php do_action('discussion_before_blog_article_closed_tag');
-      get_template_part( 'sidebar/template-ads-mobile', 'page' );
+      get_template_part( 'sidebar/template-ads', 'page' );
       get_template_part( 'sidebar/template-social-share', 'page' );
       
       if(get_current_blog_id()!=1){  // Remove ratings from main site
         get_template_part( 'sidebar/template-rate-this-article', 'page' );
       }
       
-      
-      get_template_part( 'sidebar/template-ads', 'page' );
       if(is_single() && get_post_type() == 'videos'){
           get_template_part( 'sidebar/template-related-articles', 'page' );
       }
