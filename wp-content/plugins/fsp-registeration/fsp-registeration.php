83914481191c47293304512e6719e01e1204db7f
Adam Doe <adoe@marketingincolor.com>
2016-08-01 10:36:42 -0400

    Updated login styles

--- wp-content/plugins/fsp-registeration/fsp-registeration.php
+++ wp-content/plugins/fsp-registeration/fsp-registeration.php
@@ -185,7 +185,7 @@ function fspr_login_form_fields() {
     ob_start();
     ?>
     <div class="login-container">
-        <h3 class="fspr_header"><?php _e('Login'); ?></h3>
+        <h3 class="fspr_header" style="text-transform: none; color: #4c4d4f; font-weight: 700;" ><?php _e('Sign in to your branch of MyEvergreenWellness.com.'); ?></h3>
 
         <?php
         // show any error messages after form submission
