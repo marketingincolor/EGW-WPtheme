8313958a328e98b7c4df6a5e91b80cbf80ea85fe
eddt <etwilbeck@marketingincolor.com>
2016-09-22 09:52:07 -0500

    fixed ad display issue on villages child theme

--- wp-content/themes/village-child/block/post-middle-adsblock.php
+++ wp-content/themes/village-child/block/post-middle-adsblock.php
@@ -32,7 +32,7 @@ if ($total_post >= $post_per_section) {
             ?>
         </div>-->
 
-        <div class="fsp-ads-homepage" id="adv_row_<?php echo $i; ?>" <?php if ($i != 1) { ?> style="display:none;clear:both" <?php } else { ?> style="clear:both" <?php } ?>>
+        <div class="fsp-ads-homepage" id="mob_adv_row_<?php echo $i; ?>" <?php if ($i != 1) { ?> style="display:none;clear:both" <?php } else { ?> style="clear:both" <?php } ?>>
 
             <ins data-revive-zoneid="3" id="zoneid" data-revive-id="0be604ef9a1ab68c1665959c06390bf9"></ins>
             <script async src="//myevergreenwellness.net/www/delivery/asyncjs.php"></script>
